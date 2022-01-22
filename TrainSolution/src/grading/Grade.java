d5a336b5e10dbd1671f043aac4e3af103734b48c
Jake Zhao <zhaojake14@gmail.com>
2022-01-22 12:46:08 -0500

    intial class declarations for grading algo

---
+++ TrainSolution/src/grading/Grade.java
@@ -0,0 +1,10 @@
+package grading;
+import java.util.ArrayList;
+
+public class Grade {
+	
+	private int curTime = 0;
+	private int[][] pArrival = new int[19][3]; //number of passengers arriving at [10min interval][station]
+	
+	public Grade()
+}
